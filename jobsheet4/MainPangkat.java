53983e242ed50738f2865cee3c04606a3b842177
nechasyifa <nechasyifasyafitri@gmail.com>
2024-03-16 21:55:01 +0700

    percobaan2

---
+++ jobsheet4/MainPangkat.java
@@ -0,0 +1,38 @@
+
+/**
+ * MainPangkat
+ */
+import java.util.Scanner;
+
+public class MainPangkat {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("===============================================");
+        System.out.print("Masukkan jumlah elemen yang dihitung: ");
+        int elemen = sc.nextInt();
+
+        Pangkat[] png = new Pangkat[elemen];
+        for (int i = 0; i < elemen; i++) {
+            png[i] = new Pangkat();
+            System.out.print("Masukkan nilai yang hendak dipangkatkan: ");
+            png[i].nilai = sc.nextInt();
+            System.out.print("Masukkan nilai pemangkat: ");
+            png[i].pangkat = sc.nextInt();
+        }
+        sc.close();
+
+        System.out.println("HASIL PANGKAT - BRUTE FORCE");
+        for (int i = 0; i < elemen; i++) {
+            System.out.println("Hasil dari " +
+                    png[i].nilai + " pangkat " + png[i].pangkat + " adalah "
+                    + png[i].pangkatBF(png[i].nilai, png[i].pangkat));
+        }
+        System.out.println("HASIL PANGKAT - DEVIDE AND CONQUER");
+        for (int i = 0; i < elemen; i++) {
+            System.out.println("Hasil dari " + png[i].nilai + " pangkat " + png[i].pangkat + " adalah "
+                    + png[i].pangkatDC(png[i].nilai, png[i].pangkat));
+        }
+    }
+}
