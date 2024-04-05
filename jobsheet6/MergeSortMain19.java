3385fcecfe6d4ba6e69bc464428675be1a79d259
nechasyifa <nechasyifasyafitri@gmail.com>
2024-04-05 21:38:58 +0700

    percobaan 3

---
+++ jobsheet6/MergeSortMain19.java
@@ -0,0 +1,15 @@
+/**
+ * MergeSortMain19
+ */
+public class MergeSortMain19 {
+    public static void main(String[] args) {
+        int data[] = {10, 40, 30, 50, 70, 20, 100, 90};
+        System.out.println("Sorting dengan merge sort");
+        MergeSorting19 mSort = new MergeSorting19();
+        System.out.println("Data awal");
+        mSort.printArray(data);
+        mSort.mergeSort(data);
+        System.out.println("Setelah diurutkan");
+        mSort.printArray(data);
+    }
+}
