c6ed8fbc1116988f7844dbd03b497a392bad2cb4
nechasyifa <nechasyifasyafitri@gmail.com>
2024-04-25 23:45:05 +0700

    modifikasi percobaan 1

--- jobsheet8/Queue.java
+++ jobsheet8/Queue.java
@@ -66,6 +66,7 @@ public class Queue {
     public void Enqueue(int dt) {
         if (isFull()) {
             System.out.println("Queue sudah penuh");
+            System.exit(0);
         } else {
             if (isEmpty()) {
                 front = rear = 0;
@@ -85,6 +86,7 @@ public class Queue {
         int dt = 0;
         if (isEmpty()) {
             System.out.println("Queue masih kosong");
+            System.exit(0);
         } else {
             dt =data[front];
             size--;
