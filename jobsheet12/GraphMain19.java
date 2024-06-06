8d7864342edc9b9d88096daf431ed59d19917c55
nechasyifa <nechasyifasyafitri@gmail.com>
2024-06-06 15:43:19 +0700

    percobaan 2

--- jobsheet12/GraphMain19.java
+++ jobsheet12/GraphMain19.java
@@ -2,6 +2,20 @@ import java.util.Scanner;
 
 public class GraphMain19 {
     public static void main(String[] args) throws Exception {
+        GraphMatriks19 gdg = new GraphMatriks19(4);
+        gdg.makeEdge(0, 1, 50);
+        gdg.makeEdge(1, 0, 60);
+        gdg.makeEdge(1, 2, 70);
+        gdg.makeEdge(2, 1, 80);
+        gdg.makeEdge(2, 3, 40);
+        gdg.makeEdge(3, 0, 90);
+        gdg.printGraph();
+        System.out.println("Hasil setelah penghapusan edge");
+        gdg.removeEdge(2, 1);
+        gdg.printGraph();
+
+        System.out.println();
+
         Graph19 gedung = new Graph19(6);
         gedung.addEdge(0, 1, 50);
         gedung.addEdge(0, 2, 100);
@@ -12,11 +26,11 @@ public class GraphMain19 {
         gedung.degree(0);
         gedung.printGraph();
 
-        gedung.removeEdge(1,3);
+        gedung.removeEdge(1, 3);
         gedung.printGraph();
 
         Scanner scanner19 = new Scanner(System.in);
-        
+
         System.out.print("Masukkan gedung asal: ");
         int asal = scanner19.nextInt();
         System.out.print("Masukkan gedung tujuan: ");
