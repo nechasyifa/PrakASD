7be005a13a867cfd97f4a3e2d4e8fd98c0782e6e
nechasyifa <nechasyifasyafitri@gmail.com>
2024-06-06 14:05:49 +0700

    percobaan 1

---
+++ jobsheet12/DoubleLinkedLists19.java
@@ -0,0 +1,175 @@
+public class DoubleLinkedLists19 {
+    Node19 head;
+    int size;
+
+    public DoubleLinkedLists19() {
+        head = null;
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void addFirst(int item, int jarak) {
+        if (isEmpty()) {
+            head = new Node19(null, item, jarak, null);
+        } else {
+            Node19 newNode = new Node19(null, item, jarak, head);
+            head.prev = newNode;
+            head = newNode;
+        }
+        size++;
+    }
+
+    public void addLast(int item, int jarak) {
+        if (isEmpty()) {
+            addFirst(item, jarak);
+        } else {
+            Node19 current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            Node19 newNode = new Node19(current, item, jarak, null);
+            current.next = newNode;
+            size++;
+        }
+    }
+
+    public void add(int item, int jarak, int index) throws Exception {
+        if (isEmpty()) {
+            addFirst(item, jarak);
+        } else if (index < 0 || index > size) {
+            throw new Exception("Nilai indeks di luar batas");
+        } else {
+            Node19 current = head;
+            int i = 0;
+            while (i < index) {
+                current = current.next;
+                i++;
+            }
+            if (current.prev == null) {
+                Node19 newNode = new Node19(null, item, jarak, current);
+                current.prev = newNode;
+                head = newNode;
+            } else {
+                Node19 newNode = new Node19(current.prev, item, jarak, current);
+                newNode.prev = current.prev;
+                newNode.next = current;
+                current.prev.next = newNode;
+                current.prev = newNode;
+            }
+        }
+        size++;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void clear() {
+        head = null;
+        size = 0;
+    }
+
+    public void print() {
+        if (!isEmpty()) {
+            Node19 tmp = head;
+            while (tmp != null) {
+                System.out.print(tmp.data + "\t");
+                tmp = tmp.next;
+            }
+            System.out.println("\nberhasil diisi");
+        } else {
+            System.out.println("Linked Lists Kosong");
+        }
+    }
+
+    public void removeFirst() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Linked List masih kosong, tidak dapat dihapus!");
+        } else if (size == 1) {
+            head = null;
+            size--;
+        } else {
+            head = head.next;
+            head.prev = null;
+            size--;
+        }
+    }
+
+    public void removeLast() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Linked List masih kosong, tidak dapat dihapus!");
+        } else if (head.next == null) {
+            head = null;
+            size--;
+            return;
+        }
+        Node19 current = head;
+        while (current.next.next != null) {
+            current = current.next;
+        }
+        current.next = null;
+        size--;
+    }
+
+    public void remove(int index) {
+        Node19 current = head;
+        while (current != null) {
+            if (current.data == index) {
+                if (current.prev != null) {
+                    current.prev.next = current.next;
+                } else {
+                    head = current.next;
+                }
+                if (current.next != null) {
+                    current.next.prev = current.prev;
+                }
+                break;
+            }
+            current = current.next;
+        }
+        size--;
+    }
+
+    public int getFirst() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Linked list kosong");
+        }
+        return head.data;
+    }
+
+    public int getLast() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Linked list kosong");
+        }
+        Node19 tmp = head;
+        while (tmp.next != null) {
+            tmp = tmp.next;
+        }
+        return tmp.data;
+    }
+
+    public int get(int index) throws Exception {
+        if (isEmpty() || index >= size) {
+            throw new Exception("Nilai indeks di luar batas");
+        }
+        Node19 tmp = head;
+        for (int i = 0; i < index; i++) {
+            tmp = tmp.next;
+        }
+        return tmp.data;
+    }
+
+    public int getJarak(int index) throws Exception {
+        if (isEmpty() || index >= size) {
+            throw new Exception("Nilai indeks di luar batas");
+        }
+        Node19 tmp = head;
+        for (int i = 0; i < index; i++) {
+            tmp = tmp.next;
+        }
+        return tmp.jarak;
+    }
+}
---
+++ jobsheet12/Graph19.java
@@ -0,0 +1,58 @@
+public class Graph19 {
+    int vertex;
+    DoubleLinkedLists19 list[];
+
+    public Graph19(int v) {
+        vertex = v;
+        list = new DoubleLinkedLists19[v];
+        for (int i = 0; i < v; i++) {
+            list[i] = new DoubleLinkedLists19();
+        }
+    }
+
+    public void addEdge(int asal, int tujuan, int jarak) {
+        list[asal].addFirst(tujuan, jarak);
+    }
+
+    public void degree(int asal) throws Exception {
+        int k, totalIn = 0, totalOut = 0;
+        for (int i = 0; i < vertex; i++) {
+            // inDegree
+            for (int j = 0; j < list[i].size(); j++) {
+                if (list[i].get(j) == asal) {
+                    ++totalIn;
+                }
+            }
+        }
+        // outDegree
+        totalOut = list[asal].size();
+
+        System.out.println("InDegree dari Gedung " + (char) ('A' + asal) + ": " + totalIn);
+        System.out.println("OutDegree dari Gedung " + (char) ('A' + asal) + ": " + totalOut);
+        System.out.println("Degree dari Gedung " + (char) ('A' + asal) + ": " + (totalIn + totalOut));
+    }
+
+    public void removeEdge(int asal, int tujuan) throws Exception {
+        list[asal].remove(tujuan);
+    }
+
+    public void removeAllEdge() {
+        for (int i = 0; i < vertex; i++) {
+            list[i].clear();
+        }
+        System.out.println("Graf berhasil dikosongkan");
+    }
+
+    public void printGraph() throws Exception {
+        for (int i = 0; i < vertex; i++) {
+            if (list[i].size() > 0) {
+                System.out.println("Gedung " + (char) ('A' + i) + " terhubung dengan ");
+                for (int j = 0; j < list[i].size(); j++) {
+                    System.out.print((char) ('A' + list[i].get(j)) + " (" + list[i].getJarak(j) + " m), ");
+                }
+                System.out.println("");
+            }
+        }
+        System.out.println("");
+    }
+}
